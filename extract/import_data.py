5e22f3dd18055e3effb4a99183d56c6de58e2c9c
Glenmond <glenmond@outlook.com>
2022-03-29 22:30:20 +0800

    Add RavenPack loader

--- extract/import_data.py
+++ extract/import_data.py
@@ -11,166 +11,37 @@ import sys
 try:
     from fomc_data.FomcStatement import FomcStatement
     from fomc_data.FomcMinutes import FomcMinutes
-
+    from news_data.News import News
 except ModuleNotFoundError: 
     from .fomc_data.FomcStatement import FomcStatement
     from .fomc_data.FomcMinutes import FomcMinutes
+    from .news_data.News import News
 
 batch_id = datetime.date.today().strftime("%y%m%d")
 fred_api = "18fb1a5955cab2aae08b90a2ff0f6e42"
 fred = Fred(api_key=fred_api)
 
-def fetch_data(ref_date = datetime.datetime.today()):
-    '''
-    Fetches data from FRED API, stores it in a dataframe. 
-    Takes as input a datetime of the latest day within the dataframe. 
-    Default value for this is today's date.
-    '''    
-    # concurrent
-    commodities_params = { 
-        'observation_start':'2003-01-02',
-        'observation_end':ref_date.strftime('%Y-%m-%d'),
-        'units':'lin',
-        'frequency':'m',
-        'aggregation_method': 'eop',   
-    }
-    commodities = ('PPIACO', commodities_params)
-
-    # one period lag 
-    # one period lag means observation starts and ends one period later
-    one_month_lag = ref_date + relativedelta(months=1)
-    real_gdp_params = { 
-        'observation_start':'2003-01-02',
-        'observation_end':one_month_lag.strftime('%Y-%m-%d'),
-        'units':'lin',
-        'frequency':'q',
-        'aggregation_method': 'eop',   
-    }
-    real_gdp = ('GDPC1', real_gdp_params)
-
-    # two period lag
-    # two period lag means observation starts and ends two periods later since we have to shift forward
-    two_month_lag = ref_date + relativedelta(months=2)
-    median_cpi_params ={ 
-        'observation_start':'2003-01-02',
-        'observation_end':two_month_lag.strftime('%Y-%m-%d'),
-        'units':'lin',
-        'frequency':'m',
-        'aggregation_method': 'eop',   
-    }
-    median_cpi = ('MEDCPIM158SFRBCLE', median_cpi_params)
-
-    # three period lag
-    # three period lag means observation starts and ends three periods later since we have to shift forward
-    three_month_lag = ref_date + relativedelta(months=3)
-    em_ratio_params = { 
-        'observation_start':'2003-01-02',
-        'observation_end':three_month_lag.strftime('%Y-%m-%d'),
-        'units':'lin',
-        'frequency':'m',
-        'aggregation_method': 'eop',   
-    }
-    em_ratio = ('EMRATIO', em_ratio_params)
-
-    # five period lag
-    five_month_lag = ref_date + relativedelta(months=5)
-    med_wages_params = { 
-        'observation_start':'2003-01-02',
-        'observation_end': five_month_lag.strftime('%Y-%m-%d'),
-        'units':'lin',
-        'frequency':'q',
-        'aggregation_method': 'eop',   
-    }
-    med_wages = ('LES1252881600Q', med_wages_params)
-
-    # 5 period lead
-    # five period lead means observation starts and ends five periods earlier since we have to shift backward
-    five_month_lead = ref_date - relativedelta(months=5)
-    maturity_minus_three_month_params = {
-        'observation_start':'2002-08-02', 
-        'observation_end':five_month_lead.strftime('%Y-%m-%d'),
-        'units':'lin',
-        'frequency':'m',
-        'aggregation_method': 'eop',
-    }
-    maturity_minus_three_month = ('T10Y3M', maturity_minus_three_month_params)
-
-    indicators = [
-        commodities,
-        real_gdp,
-        median_cpi,
-        em_ratio,
-        med_wages,
-        maturity_minus_three_month,
-    ]
-
-    df = pd.DataFrame()
-
-    # target value
-    fed_fund_date = ref_date-relativedelta(months=1) # get the latest available end-of-month data for this
-    fed_fund_rate = fred.get_series(
-        "DFF",
-        **{
-            "observation_start": "2003-01-02", 
-            "observation_end": fed_fund_date.strftime('%Y-%m-%d'), 
-            "frequency": "m",
-            "aggregation_method": "eop",
-        }
-    )
-
-    fed_fund_rate.index = pd.to_datetime(fed_fund_rate.index).to_period("M")
-    df["target"] = fed_fund_rate.to_numpy()
-    df.index = fed_fund_rate.index
-    for series_id, params in indicators:
-        # Get the data from FRED, convert to pandas DataFrame
-        indicator = fred.get_series(series_id, **params)
-        indicator = indicator.to_frame().set_axis([series_id], axis="columns")
-        # fill in data with '0.0' that is presented as just '.'
-        indicator[series_id] = ["0.0" if x == "." else x for x in indicator[series_id]]
-        # turn the value into numeric
-        indicator[series_id] = pd.to_numeric(indicator[series_id])
-        indicator.index = pd.to_datetime(indicator.index).to_period("M")
-        indicator = indicator.resample("M").interpolate()
-
-        if series_id in ("PAYEMS", "T5YIE", "GDPC1"):  # align 1 lag
-            indicator = indicator.shift(-1)[:-1]
-
-        if series_id in ("MEDCPIM158SFRBCLE"):  # align 2 lag
-            indicator = indicator.shift(-2)[:-2]
-            indicator.rename(columns={"MEDCPIM158SFRBCLE": "MEDCPI"}, inplace=True)
-
-        if series_id in ("EMRATIO"):  # align 3 lag
-            indicator = indicator.shift(-3)[:-3]
-
-        if series_id in ("LES1252881600Q"):  # align 5 lag
-            indicator = indicator.shift(-5)[:-5]
-            indicator.rename(columns={"LES1252881600Q": "MEDWAGES"}, inplace=True)
-
-        if series_id in ("PERMIT", "AMTMNO", "DGORDER", "T10Y3M"):  # align 5 lead
-            indicator = indicator.shift(5)[5:]
-
-        # join the dataframes together
-        df = pd.concat([indicator, df], axis="columns")
-    return df
-
-
 def download_data(docs, from_year):
-    df = docs.get_contents(from_year)
-    print("Shape of the downloaded data: ", df.shape)
-    print("The first 5 rows of the data: \n", df.head())
-    print("The last 5 rows of the data: \n", df.tail())
-    docs.pickle_dump_df(filename=f"{batch_id}_{docs.content_type}" + ".pickle")
+    if docs.content_type == "news":
+        news_sources_df, news_volume_spikes_df = docs.get_contents(from_year)
+        print("Shape of the news sources downloaded data: ", news_sources_df.shape)
+        print("The first 5 rows of the data: \n", news_sources_df.head())
+        print("The last 5 rows of the data: \n", news_sources_df.tail())
 
+        print("Shape of the news volumes spikes downloaded data: ", news_volume_spikes_df.shape)
+        print("The first 5 rows of the data: \n", news_volume_spikes_df.head())
+        print("The last 5 rows of the data: \n", news_volume_spikes_df.tail())
 
-def download_fed_futures_historical(path):
-    # Futures: full historical + forward data
-    # download from barchart
-    full_path = path + "/raw_data/historical-prices.csv"
-    futures = pd.read_csv(full_path)
-    futures = futures[:-1]
-    futures['Exp Date'] = pd.to_datetime(futures['Exp Date'])
-    futures = futures.set_index("Exp Date")    
-    return futures
+        docs.pickle_dump_df(f"{batch_id}_{docs.content_type}_sources" + ".pickle", "news_sources_df")
+        docs.pickle_dump_df(f"{batch_id}_{docs.content_type}_volume_spikes" + ".pickle", "news_volume_spikes")
+
+    else:
+        df = docs.get_contents(from_year)
+        print("Shape of the downloaded data: ", df.shape)
+        print("The first 5 rows of the data: \n", df.head())
+        print("The last 5 rows of the data: \n", df.tail())
+        docs.pickle_dump_df(filename=f"{batch_id}_{docs.content_type}" + ".pickle")
+
 
 def download_fomc_dates():
         page = requests.get("https://www.federalreserve.gov/monetarypolicy/fomccalendars.htm")
@@ -205,7 +76,7 @@ def download_fomc_dates():
         return df
 
 def import_sentiment_data(from_year, content_type='all', base_dir='../data/sentiment_data/extract/'):
-    content_type_all = ("statement", "minutes", "all")
+    content_type_all = ("statement", "minutes", "news", "all")
 
     if content_type not in content_type_all:
         print("Please specify the first argument from ", content_type_all)
@@ -220,13 +91,16 @@ def import_sentiment_data(from_year, content_type='all', base_dir='../data/senti
         download_data(fomc, from_year)
         fomc = FomcMinutes(base_dir=base_dir)
         download_data(fomc, from_year)
+        news = News(base_dir=base_dir)
+        download_data(news, from_year)
         
     else:
         if content_type == "statement":
             docs = FomcStatement()
         elif content_type == "minutes":
             docs = FomcMinutes()
-
+        else:  # News
+            docs = News()
         download_data(docs, from_year)
     print("Sentiment data imported")
 
@@ -234,7 +108,7 @@ if __name__ == "__main__":
 
     pg_name = sys.argv[0]
     args = sys.argv[1:]
-    content_type_all = ('statement', 'minutes', 'all')
+    content_type_all = ('statement', 'minutes', 'news', 'all')
 
     if (len(args) != 1) and (len(args) != 2):
         print("Usage: ", pg_name)
--- extract/news_data/News.py
+++ extract/news_data/News.py
@@ -1,129 +1,102 @@
-import re
-import threading
-import pickle
 import os
+import json
+import time
+import requests
+from datetime import datetime
+import dateutil
 import pandas as pd
+import pickle
+import numpy as np
+# import datetime
+from ravenpackapi import RPApi
 
-from abc import ABCMeta, abstractmethod
+# apikey = RPApi(api_key='INSERT YOUR API KEY')
+
+news_sources = {
+    'dbs_general': "43DF4D8FA22B80ECE94E8729BEDEC6C4",
+    'uob_general': "FD5AA5E1DBD987CE34BED42728ADCCE4",
+    'ocbc_general': "4B27B6E1B37DDBF8CA6604646B42B8DD",
+}
+
+news_volume_spikes = {
+    'dbs_news_volume_spikes': "B25D42AF9042C76574B87E4ABDE66B81",
+    'uob_news_volume_spikes': "7698BF1230711675A6ED9A53A21514A5",
+    'ocbc_news_volume_spikes': "7FF80DA8AFB3C36F7DCFA2BE2D0EC364"
+}
 
 class News():
-    def __init__(self, content_type, verbose, max_threads, base_dir):
+    def __init__(self, base_dir='../data/sentiment_data/extract/'):
         # Set arguments to internal variables
-        self.content_type = content_type
-        self.verbose = verbose
-        self.MAX_THREADS = max_threads
+        self.content_type = "news"
         self.base_dir = base_dir
+        self.news_sources_df = None
+        self.news_volume_spikes_df = None
+        self.start = None
+        self.end = None
 
-        # Initialization
-        self.df = None
-        self.links = None
-        self.dates = None
-        self.articles = None
-        self.speakers = None
-        self.titles = None
+    def download_data(self, dataset_id, start_date, end_date):
+        """
+        Self-improvised methods from RavenPack API
+        Limitation of API: only 1 year or 10,000 worth of datasets allowed per requests
+        """
 
-        # FOMC website URLs
-        self.base_url = 'https://www.federalreserve.gov'
-        self.calendar_url = self.base_url + '/monetarypolicy/fomccalendars.htm'
+        ds = apikey.get_dataset(dataset_id)
+        data = ds.json(
+            start_date,
+            end_date,
+        )
 
-        # FOMC Chairperson's list
-        self.chair = pd.DataFrame(
-            data=[["Greenspan", "Alan", "1987-08-11", "2006-01-31"], 
-                  ["Bernanke", "Ben", "2006-02-01", "2014-01-31"], 
-                  ["Yellen", "Janet", "2014-02-03", "2018-02-03"],
-                  ["Powell", "Jerome", "2018-02-05", "2022-02-05"]],
-            columns=["Surname", "FirstName", "FromDate", "ToDate"])
-        
-    def _date_from_link(self, link):
-        date = re.findall('[0-9]{8}', link)[0]
+        list_of_dict = []
+        for record in data:
+            list_of_dict.append(record)
 
-        if date[4] == '0':
-            date = "{}-{}-{}".format(date[:4], date[5:6], date[6:])
-        else:
-            date = "{}-{}-{}".format(date[:4], date[4:6], date[6:])
-
-        return date
-
-    def _speaker_from_date(self, article_date):
-        if self.chair.FromDate[0] < article_date and article_date < self.chair.ToDate[0]:
-            speaker = self.chair.FirstName[0] + " " + self.chair.Surname[0]
-        elif self.chair.FromDate[1] < article_date and article_date < self.chair.ToDate[1]:
-            speaker = self.chair.FirstName[1] + " " + self.chair.Surname[1]
-        elif self.chair.FromDate[2] < article_date and article_date < self.chair.ToDate[2]:
-            speaker = self.chair.FirstName[2] + " " + self.chair.Surname[2]
-        elif self.chair.FromDate[3] < article_date and article_date < self.chair.ToDate[3]:
-            speaker = self.chair.FirstName[3] + " " + self.chair.Surname[3]
-        else:
-            speaker = "other"
-        return speaker
-
-    def _get_articles_multi_threaded(self):
-        '''
-        Get all articles using multi-threading
-        '''
-        if self.verbose:
-            print("Getting articles - Multi-threaded...")
-
-        self.articles = ['']*len(self.links)
-        jobs = []
-        # initiate and start threads:
-        index = 0
-        while index < len(self.links):
-            if len(jobs) < self.MAX_THREADS:
-                t = threading.Thread(target=self._add_article, args=(self.links[index],index,))
-                jobs.append(t)
-                t.start()
-                index += 1
-            else:    # wait for threads to complete and join them back into the main thread
-                t = jobs.pop(0)
-                t.join()
-        for t in jobs:
-            t.join()
-
-    @abstractmethod
-    def _get_links(self, from_year):
-        '''
-        Sets all the links for the FOMC meetings
-        from the giving from_year to the current most recent year
-        from_year is min(2015, from_year)
-        '''
-        # Implement in sub classes
-        pass
+        return pd.DataFrame.from_dict(list_of_dict, orient='columns')
     
-    @abstractmethod
-    def _add_article(self, link, index=None):
-        '''
-        Adds the related article for 1 link into the instance variable
-        index is the index in the article to add to. 
-        '''
-        # Implement in sub classes
-        pass
+    def get_contents(self, from_year):
+        '''Sends and parses request/response to/from NYT Archive API for given dates.'''
+        
+        # Set arguments to internal variables
+        self.end = datetime.date(datetime.today())
+        self.start = datetime.date(datetime(from_year, 1, 1))
 
-    def get_contents(self, from_year=1990):
-        '''
-        Returns a df with the date as the index.
-        Save the same to internal df as well.
-        '''
-        self._get_links(from_year)
-        self._get_articles_multi_threaded()
-        dict = {
-            'date': self.dates,
-            'contents': self.articles,
-            'speaker': self.speakers, 
-            'title': self.titles
-        }
-        self.df = pd.DataFrame(dict).sort_values(by=['date'])
-        self.df.reset_index(drop=True, inplace=True)
-        return self.df
+        dates = [x.split(' ') for x in pd.date_range(self.start, self.end, freq='MS').strftime("%Y %-m").tolist()]
+        print("Getting articles for news..." + 'Date range: ' + str(dates[0]) + ' to ' + str(dates[-1]))
+        
+        ldf = []
+        for k, v in news_sources.items():
+            ldf.append(self.download_data(v, self.start, self.end))
+        news_sources_df = pd.concat(ldf, axis=0)
+        news_sources_df.reset_index(inplace=True, drop=True)
+        
+        print("Getting articles for news volume spikes..." + 'Date range: ' + str(dates[0]) + ' to ' + str(dates[-1]))
+        ldf = []
+        for k, v in news_volume_spikes.items():
+            ldf.append(self.download_data(v, self.start, self.end))
+        news_volume_spikes_df = pd.concat(ldf, axis=0)
+        news_volume_spikes_df.reset_index(inplace=True, drop=True)
 
-    def pickle_dump_df(self, filename="output.pickle"):
+        # Assign to attributes
+        self.news_sources_df = news_sources_df
+        self.news_volume_spikes_df = news_volume_spikes_df
+
+        return news_sources_df, news_volume_spikes_df
+
+    
+    def pickle_dump_df(self, filename="output.pickle", filetype="news_sources_df"):
         '''
         Dump df to a pickle file
         '''
+        if filetype == "news_sources_df":
+            df = self.news_sources_df
+        else:
+            df = self.news_volume_spikes_df
+
+
         filepath = self.base_dir + filename
         print("")
-        if self.verbose:
-            print("Writing to ", filepath)
+        print("Writing to ", filepath)
         os.makedirs(os.path.dirname(filepath), exist_ok=True)
         with open(filepath, "wb") as output_file:
-            pickle.dump(self.df, output_file)
+            pickle.dump(df, output_file)
+
+        
