3cd07a50bb9b6437dd2bfc6d190fa2d8497bf9ae
zhengyiwong <51377600+zhengyiwong@users.noreply.github.com>
2022-03-31 19:34:55 +0800

    Update S_ALL_PRICE.sql

--- airflow/dags/sql/S_ALL_PRICE.sql
+++ airflow/dags/sql/S_ALL_PRICE.sql
@@ -17,6 +17,21 @@ LEFT JOIN
 `{{ params.project_id }}.{{ params.staging_source_dataset }}.EXCHANGE_RATE_STAGING` er
 ON
 er.Date = price.Date
+UNION ALL
+SELECT
+price.Date AS Date,
+price.Ticker AS Ticker_id,
+price.Name AS Name_id,
+price.High AS High,
+price.Low AS Low,
+price.Open AS Open,
+price.Close AS Close,
+price.Adj_Close AS Adj_close,
+price.Volume AS Volume,
+1 AS Conversion_factor,
+"SGD" AS Exchange_rate_ticker
+FROM
+`{{ params.project_id }}.{{ params.staging_source_dataset }}.PRICE_STAGING` price
 ORDER BY
-DATE,
-Ticker_id;
+Date,
+Ticker_id DESC
